f0a1d1cfce71c47183a3bff9475fa2a38b142c6a
Wesley Santos <wesleysotnas64@gmail.com>
2024-12-14 10:08:16 -0300

    Commit to backup.

--- Design Patterns/obj/Debug/net6.0/Singleton.AssemblyInfo.cs
+++ Design Patterns/obj/Debug/net6.0/Singleton.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Singleton")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+973f39a83ad90bff36c4e1166b24af59d03e412c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Singleton")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Singleton")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
